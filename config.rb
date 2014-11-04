a8bb2f2f3bbc73c4a137d06c41d0985bfad0e5a5
Mike Tigas <mike@tig.as>
2014-11-04 13:40:56 -0500

    config

--- config.rb
+++ config.rb
@@ -20,18 +20,21 @@ end
 ###
 # Config
 ###
-set :css_dir, 'stylesheets'
-set :js_dir, 'javascripts'
-set :images_dir, 'images'
+set :css_dir, '/stylesheets'
+set :js_dir, '/javascripts'
+set :images_dir, '/images'
 set :url_prefix, '/'
-set :absolute_prefix, 'http://localhost:4567'
+set :absolute_prefix, '/'
 
 # Build-specific configuration
 configure :build do
   puts "local build"
-  set :url_prefix, ""
-  set :absolute_prefix, ""
-  activate :asset_hash
+  set :css_dir, '/meme/stylesheets'
+  set :js_dir, '/meme/javascripts'
+  set :images_dir, '/meme/images'
+  set :url_prefix, "/meme"
+  set :absolute_prefix, "http://tools.propublica.local/meme"
+  #activate :asset_hash
   activate :minify_javascript
   activate :minify_css
 end
